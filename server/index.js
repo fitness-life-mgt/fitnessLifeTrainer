de918135e73f692f75bdc895c72cc5c9bbc4821a
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2021-09-14 17:05:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ app.use("/register",require("./routes/register"));
 app.use("/login",require("./routes/login"));
 app.use("/editprofile",require("./routes/editProfile"));
 app.use("/memberList",require("./routes/memberList"));
+app.use("/approve",require("./routes/approve"));
+app.use("/approvediet",require("./routes/approvediet.js"));
 
 app.listen(8088, () => {
     console.log("Yey, your server is running on port 8088");
